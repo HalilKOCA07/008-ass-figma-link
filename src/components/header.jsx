137c0351dc4d5bb012a6ff134485d58ffab24e9d
Sinyour <sinyour52@gmail.com>
2024-04-22 09:21:14 +0300

    Header done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,18 @@
 const Header = () => {
 
     return(
-<div className="navbar">
-	<div className="navbar-start">
-		<a className="navbar-item">Ripple UI</a>
+<div className="navbar px-10 h-10 shadow-none mt-5">
+	<div className="flex flex-nowrap w-1/6 text-lg text-2xl me-10">
+		<a className="navbar-item">CO-developer</a>
 	</div>
-	<div className="navbar-center">
-		<a className="navbar-item">Home</a>
-		<a className="navbar-item">About</a>
-		<a className="navbar-item">Contact</a>
+	<div className="">
+		<a className="navbar-item hover:bg-blue-100">Home</a>
+		<a className="navbar-item hover:bg-blue-100">About</a>
+		<a className="navbar-item hover:bg-blue-100">Contact</a>
 	</div>
 	<div className="navbar-end">
-		<a className="navbar-item">Home</a>
+		<a className="navbar-item hover:bg-blue-100">Sign in</a>
+		<a className="h-10 flex justify-center px-3 pt-2 rounded-lg bg-blue-800 text-white hover:bg-blue-900">Create free account</a>
 	</div>
 </div>
     )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div>
+        <div></div>
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default Home
